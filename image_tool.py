f40fdba113914b0a49660ff995aaad3ebda4adf0
Rudy <63475761+rudymohammadbali@users.noreply.github.com>
2024-07-03 02:45:49 +0200

    Update image_tool.py
    Added image merge function:
    merge_images_horizontally()
    merge_images_vertically()

--- image_tool.py
+++ image_tool.py
@@ -94,3 +94,125 @@ def compress_image(input_path: str, output_path: str, quality: int) -> bool:
     except Exception as e:
         print(f"Error compressing {input_path}: {e}")
         return False
+
+
+def merge_images_horizontally(images: list, width: int, height: int, output_path: str, output_format: str) -> bool:
+    """
+       Merges a list of images horizontally into a single image.
+
+       Args:
+           images (list): List of image file paths.
+           width (int): Desired width for each image.
+           height (int): Desired height for each image.
+           output_path (str): Path to the output folder where the merged image will be saved.
+           output_format (str): Desired output image format (e.g., "jpeg", "png", "gif").
+
+       Raises:
+           NotADirectoryError: If the output folder does not exist.
+           UnsupportedImageFormatError: If the specified output format is not supported.
+
+       Returns:
+           bool: True if the merge was successful, False otherwise.
+       """
+    total_width = 0
+    max_height = 0
+    valid_images = []
+    image_formats = [
+        "jpeg",
+        "png",
+        "gif",
+        "bmp",
+        "tiff",
+        "webp",
+        "ico"
+    ]
+
+    if not path_exists(output_path):
+        raise NotADirectoryError(f"Output folder {output_path} does not exists.")
+
+    if output_format.lower() not in image_formats:
+        raise UnsupportedImageFormatError(output_format)
+
+    try:
+        for img_path in images:
+            if not is_file(img_path):
+                print(f"Image {img_path} does not exists!")
+            else:
+                img = Image.open(img_path).resize(size=(width, height))
+                valid_images.append(img)
+                total_width += width
+                max_height = max(max_height, height)
+
+        merge_image = Image.new("RGB", (total_width, max_height), "white")
+        x_offset = 0
+
+        for img in valid_images:
+            merge_image.paste(img, (x_offset, 0))
+            x_offset += img.width
+
+        merge_image.save(f"{output_path}\\merged_images_horizontally.{output_format}", format=output_format)
+        return True
+    except Exception as e:
+        print(f"Error merging {valid_images}: {e}")
+        return False
+
+
+def merge_images_vertically(images: list, width: int, height: int, output_path: str, output_format: str) -> bool:
+    """
+    Merges a list of images vertically into a single image.
+
+    Args:
+        images (list): List of image file paths.
+        width (int): Desired width for each image.
+        height (int): Desired height for each image.
+        output_path (str): Path to the output folder where the merged image will be saved.
+        output_format (str): Desired output image format (e.g., "jpeg", "png", "gif").
+
+    Raises:
+        NotADirectoryError: If the output folder does not exist.
+        UnsupportedImageFormatError: If the specified output format is not supported.
+
+    Returns:
+        None: The vertically merged image is saved to the specified output path.
+    """
+    total_height = 0
+    max_width = 0
+    valid_images = []
+    image_formats = [
+        "jpeg",
+        "png",
+        "gif",
+        "bmp",
+        "tiff",
+        "webp",
+        "ico"
+    ]
+
+    if not path_exists(output_path):
+        raise NotADirectoryError(f"Output folder {output_path} does not exist.")
+
+    if output_format.lower() not in image_formats:
+        raise UnsupportedImageFormatError(output_format)
+
+    try:
+        for img_path in images:
+            if not is_file(img_path):
+                print(f"Image {img_path} does not exist!")
+            else:
+                img = Image.open(img_path).resize(size=(width, height))
+                valid_images.append(img)
+                total_height += height
+                max_width = max(max_width, width)
+
+        merge_image = Image.new("RGB", (max_width, total_height), "white")
+        y_offset = 0
+
+        for img in valid_images:
+            merge_image.paste(img, (0, y_offset))
+            y_offset += img.height
+
+        merge_image.save(f"{output_path}\\merged_images_vertically.{output_format}", format=output_format)
+        return True
+    except Exception as e:
+        print(f"Error merging {valid_images}: {e}")
+        return False
